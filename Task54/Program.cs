7f921466f58b39e480930478b1ec50b2f193acc4
Kikot Ekaterina <kati-kik@yandex.ru>
2023-06-13 12:12:41 +0300

    решена задача 54

---
+++ Task54/Program.cs
@@ -0,0 +1,49 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        matrix[i, j] = new Random().Next(-30, 31);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        Console.Write($"{matrix[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+void SortDescending(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int row = j; row < matrix.GetLength(1); row++)
+            {
+                if (matrix[i, j] < matrix[i, row])
+                {
+                    int temp = matrix[i, row];
+                    matrix[i, row] = matrix[i, j];
+                    matrix[i, j] = temp;
+                }
+            }
+
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальный массив:");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+SortDescending(matrix);
+Console.WriteLine("Сортировка по убыванию:");
+PrintMatrix(matrix);
